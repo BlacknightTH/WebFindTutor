bee141a30419b46622dafbd5f2f6e31058139f02
วีรภัทร กอบกาญจนา <top4game@hotmail.com>
2018-06-07 14:43:52 +0700

    Hell

---
+++ WebFindTutor_Client/GlobalVariables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace WebFindTutor_Client
+{
+    public class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        private GlobalVariables()
+        {
+            WebApiClient.BaseAddress = new Uri("http://localhost:2930/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
