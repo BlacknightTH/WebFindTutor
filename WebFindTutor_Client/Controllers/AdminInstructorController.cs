21a7d46b033deec758f874a585bbaa6dd606a0f4
วีรภัทร กอบกาญจนา <top4game@hotmail.com>
2018-06-09 23:48:51 +0700

    TYU

---
+++ WebFindTutor_Client/Controllers/AdminInstructorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebFindTutor_Client.Models;
+
+namespace WebFindTutor_Client.Controllers
+{
+    public class AdminInstructorController : Controller
+    {
+        // GET: AdminInstructor
+        public ActionResult Index()
+        {
+            IEnumerable<InstructorModel> istList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("INSTRUCTORs").Result;
+            istList = response.Content.ReadAsAsync<IEnumerable<InstructorModel>>().Result;
+            return View(istList);
+        }
+    }
+}
